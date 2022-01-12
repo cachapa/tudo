b961b948151187c58393752372998b532b7e1abc
Daniel Cachapa <cachapa@gmail.com>
2022-01-12 13:51:44 +0100

    iOS release script

---
+++ .github/scripts/import-profile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/tudo.mobileprovision
+
