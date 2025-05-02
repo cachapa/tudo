2815ea9e250ea049314bcfa30640baaac0487f5e
Daniel Cachapa <cachapa@gmail.com>
2025-05-02 20:50:29 +0200

    Add launcher shortcuts

--- lib/lists/home_page.dart
+++ lib/lists/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quick_actions/quick_actions.dart';
 
 import '../common/master_detail.dart';
 import '../extensions.dart';
@@ -17,14 +18,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ToDoList? _list;
 
+  final _quickActions = const QuickActions();
+
   @override
   void initState() {
     super.initState();
 
+    _quickActions.initialize((listId) async {
+      final list = await Registry.listProvider.getList(listId).first;
+      setState(() => _list = list);
+    });
+
     Registry.listProvider.lists.listen((lists) {
+      // Detect when the currently selected list is deleted
       if (_list != null && !lists.map((e) => e.id).contains(_list!.id)) {
         setState(() => _list = null);
       }
+
+      // Populate quick actions menu
+      _quickActions.setShortcutItems(lists
+          .map((list) => ShortcutItem(
+              type: list.id, localizedTitle: list.name, icon: 'list_shortcut'))
+          .toList());
     });
   }
 
